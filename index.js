d9f3c67c80ab24d2c3fea78a5b0b2624187b5946
mvh2016 <58753975+mvh2016@users.noreply.github.com>
2020-02-10 16:31:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,16 @@ client.on('message', message => {
             .setDescription('Gives you a list of commands that I accept!')
             .addField('`/btcheck`', 'Puts out the basic training event check message.', false)
             .addField('`/stcheck`', 'Puts out the scenario training event check message.', false)
-            .addField('`/dtcheck`', 'Puts out the discipline training event check message.', false)
+            .addField('`/btcheck`', 'Puts out the discipline training event check message.', false)
             .addField('`/btstart <time starting>`', 'Announces a basic training that is starting! Make sure to provide the starting time and timezone. \ni.e. /btstart 18:00 GMT', false)
             .addField('`/ststart <time starting>`', 'Announces a scenarios training that is starting! Make sure to provide the starting time and timezone. \ni.e. /ststart 18:00 GMT', false)
             .addField('`/dtstart <time starting>`', 'Announces a discipline training that is starting! Make sure to provide the starting time and timezone. \ni.e. /dtstart 18:00 GMT', false)
             .addField('`/btend`', 'Announces the end of a basic training.', false)
             .addField('`/stend`', 'Announces the end of a scenarios training.', false)
             .addField('`/dtend`', 'Announces the end of a discipline training.', false)
+            .addField('`/recruitment [time]`', 'Schedules a recruitment session. \ni.e. /recruitment 18:00 EST', false)
+            .addField('`/recruitment start`', 'Announces the start of a recruitment session.', false)
+            .addField('`/recruitment end`', 'Announces the end of a recruitment session.')
             .setFooter('Bot created by CanadianJudgement | Judgement#3155 for help')
         message.channel.send(ThisEmbed)
     }
@@ -256,6 +259,54 @@ client.on('message', message => {
         else {
             message.channel.send(':warning: Insufficient permissions - role required: `Event Host` '+message.author)
         }
+    }
+
+    if (message.content.startsWith(`${prefix}recruitment`)) {
+        var thisArg = message.content.split(' ')
+        thisArg.shift()
+        if (thisArg.length > 1) {
+            var finalArg = thisArg.join(' ')
+            // Assuming that this is the scheduling command
+            const rsSchedule = new Discord.RichEmbed()
+                .setTitle(':calendar_spiral: Recruitment Session Scheduled :calendar_spiral:')
+                .setDescription('A recruitment session will occur at the following time;')
+                .addField('Time', finalArg, false)
+                .addField('Host', message.author, false)
+                .setFooter('Bot created by CanadianJudgement | Judgement#3155 for help')
+            var channelToSend = client.channels.get('673861759509069834')
+            channelToSend.send(rsSchedule)
+            message.channel.send(':white_check_mark: Event schedule message successfully posted, '+message.author)
+            channelToSend.send('<@&672798621678305290>')
+        }
+        else if (thisArg.length === 1) {
+            var lowercase = thisArg.toString().toLowerCase();
+            if (lowercase === 'start') {
+                const rsStart = new Discord.RichEmbed()
+                    .setTitle('Recruitment Session Starting')
+                    .setDescription('A recruitment session is starting now!')
+                    .addField('Host', message.author, false)
+                    .setFooter('Bot created by CanadianJudgement | Judgement#3155 for help')
+                var channelToSend = client.channels.get('673861759509069834')
+                channelToSend.send(rsStart)
+                message.channel.send(':white_check_mark: Event start message successfully posted, '+message.author)
+                channelToSend.send('<@&672798621678305290>')
+            }
+            else if (lowercase === 'end') {
+                const rsEnd = new Discord.RichEmbed()
+                    .setTitle('Recruitment Session Ended')
+                    .setDescription('The recruitment session has ended.')
+                    .setFooter('Bot created by CanadianJudgement | Judgement#3155 for help')
+                var channelToSend = client.channels.get('673861759509069834')
+                channelToSend.send(rsEnd)
+                message.channel.send(':white_check_mark: Event start message successfully posted, '+message.author)
+            }
+            else {
+                message.channel.send(':warning: Invalid argument. Try `start`, `end`, or schedule a time. View /commands for more info. '+message.author)
+            }}
+        else if (thisArg.length === 0) {
+            message.channel.send(':warning: Provide an argument - `start`, `end`, or schedule a time. View /commands for more info. '+message.author)
+        }
+
     }
 
 })
